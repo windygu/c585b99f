25c87b935a858472b2279574a579d538693f1ac1
wxll1120 <wxll1120@gmail.com>
2015-09-28 07:55:33 +0800

    修改DataAccessBase

--- CarManage/CarManage.DataAccess/DataAccessBase.cs
+++ CarManage/CarManage.DataAccess/DataAccessBase.cs
@@ -80,7 +80,6 @@ namespace CarManage.DataAccess.MySql
             return new MySqlCommand(commandText, conn);
         }
 
-
         protected IDbCommand CreateCommand(string commandText, IDbConnection connection, IDbTransaction transaction)
         {
             MySqlConnection conn = connection as MySqlConnection;
@@ -90,15 +89,389 @@ namespace CarManage.DataAccess.MySql
         }
 
 
-        protected void ExecuteNonQuery(string commandText, IDbConnection connection)
+        protected int ExecuteNonQuery(string commandText, string connectionString)
         {
-            //connection.exe
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                IDbCommand command = new MySqlCommand(commandText);
+                command.Connection = connection;
+
+                return command.ExecuteNonQuery();
+            }
         }
 
-        protected void ExecuteNonQuery(IDbCommand command)
+        protected int ExecuteNonQuery(IDbCommand command)
         {
-            command.ExecuteNonQuery();
-            //new MySqlCommand()
+            if (command.Connection == null)
+            {
+                using (IDbConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    command.Connection = connection;
+
+                    return command.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                return command.ExecuteNonQuery();
+            }
         }
+
+        protected int ExecuteNonQuery(string connectionString, IDbCommand command)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                command = new MySqlCommand();
+                command.Connection = connection;
+
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        protected int ExecuteNonQuery(string commandText, IDbConnection connection)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            IDbCommand command = new MySqlCommand(commandText);
+            command.Connection = connection;
+
+            return command.ExecuteNonQuery();
+        }
+
+        protected int ExecuteNonQuery(IDbConnection connection, IDbCommand command)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            command = new MySqlCommand();
+            command.Connection = connection;
+
+            return command.ExecuteNonQuery();
+        }
+
+        protected int ExecuteNonQuery(string commandText, IDbConnection connection, IDbTransaction transaction)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            if (transaction == null)
+                transaction = connection.BeginTransaction();
+
+            IDbCommand command = new MySqlCommand(commandText);
+            command.Connection = connection;
+            command.Transaction = transaction;
+
+            return command.ExecuteNonQuery();
+        }
+
+        protected int ExecuteNonQuery(IDbConnection connection, IDbTransaction transaction, IDbCommand command)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            if (transaction == null)
+                transaction = connection.BeginTransaction();
+
+            command.Connection = connection;
+            command.Transaction = transaction;
+
+            return command.ExecuteNonQuery();
+        }
+
+        protected int Execute(string commandText, IDbConnection connection, IDbTransaction transaction = null,
+            int? commandTimeout = null, CommandType? commandType = null, object param = null)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            return connection.Execute(commandText, param, transaction, commandTimeout, commandType);
+        }
+
+        protected object ExecuteScalar(string commandText, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                IDbCommand command = new MySqlCommand(commandText);
+                command.Connection = connection;
+
+                return command.ExecuteScalar();
+            }
+        }
+
+        protected object ExecuteScalar(IDbCommand command)
+        {
+            if (command.Connection == null)
+            {
+                using (IDbConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    command.Connection = connection;
+
+                    return command.ExecuteScalar();
+                }
+            }
+            else
+            {
+                return command.ExecuteScalar();
+            }
+        }
+
+        protected object ExecuteScalar(string connectionString, IDbCommand command)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                command = new MySqlCommand();
+                command.Connection = connection;
+
+                return command.ExecuteScalar();
+            }
+        }
+
+        protected object ExecuteScalar(string commandText, IDbConnection connection)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            IDbCommand command = new MySqlCommand(commandText);
+            command.Connection = connection;
+
+            return command.ExecuteScalar();
+        }
+
+        protected object ExecuteScalar(IDbConnection connection, IDbCommand command)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            command = new MySqlCommand();
+            command.Connection = connection;
+
+            return command.ExecuteScalar();
+        }
+
+        protected object ExecuteScalar(string commandText, IDbConnection connection, IDbTransaction transaction)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            if (transaction == null)
+                transaction = connection.BeginTransaction();
+
+            IDbCommand command = new MySqlCommand(commandText);
+            command.Connection = connection;
+            command.Transaction = transaction;
+
+            return command.ExecuteScalar();
+        }
+
+        protected object ExecuteScalar(IDbConnection connection, IDbTransaction transaction, IDbCommand command)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            if (transaction == null)
+                transaction = connection.BeginTransaction();
+
+            command.Connection = connection;
+            command.Transaction = transaction;
+
+            return command.ExecuteScalar();
+        }
+
+        public object ExecuteObject(string commandText, IDbConnection connection, IDbTransaction transaction = null,
+            int? commandTimeout = null, CommandType? commandType = null, object param = null)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            return connection.ExecuteScalar(commandText, param, transaction, commandTimeout, commandType);
+        }
+
+        public T ExecuteObject<T>(string commandText, IDbConnection connection, IDbTransaction transaction = null,
+           int? commandTimeout = null, CommandType? commandType = null, object param = null)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            return connection.ExecuteScalar<T>(commandText, param, transaction, commandTimeout, commandType);
+        }
+
+        protected IDataReader ExecuteReader(string commandText, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                IDbCommand command = new MySqlCommand(commandText);
+                command.Connection = connection;
+
+                return command.ExecuteReader();
+            }
+        }
+
+        protected IDataReader ExecuteReader(IDbCommand command)
+        {
+            if (command.Connection == null)
+            {
+                using (IDbConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    command.Connection = connection;
+
+                    return command.ExecuteReader();
+                }
+            }
+            else
+            {
+                return command.ExecuteReader();
+            }
+        }
+
+        protected IDataReader ExecuteReader(string connectionString, IDbCommand command)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                command = new MySqlCommand();
+                command.Connection = connection;
+
+                return command.ExecuteReader();
+            }
+        }
+
+        protected IDataReader ExecuteReader(string commandText, IDbConnection connection)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            IDbCommand command = new MySqlCommand(commandText);
+            command.Connection = connection;
+
+            return command.ExecuteReader();
+        }
+
+        protected IDataReader ExecuteReader(IDbConnection connection, IDbCommand command)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            command = new MySqlCommand();
+            command.Connection = connection;
+
+            return command.ExecuteReader();
+        }
+
+        protected IDataReader ExecuteReader(IDbConnection connection, IDbTransaction transaction, IDbCommand command)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            if (transaction == null)
+                transaction = connection.BeginTransaction();
+
+            command.Connection = connection;
+            command.Transaction = transaction;
+
+            return command.ExecuteReader();
+        }
+
+        public IDataReader ExecuteResult(string commandText, IDbConnection connection, IDbTransaction transaction = null,
+            int? commandTimeout = null, CommandType? commandType = null, object param = null)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            return connection.ExecuteReader(commandText, param, transaction, commandTimeout, commandType);
+        }
+
+        public IEnumerable<T> Query<T>(string commandText, IDbConnection connection, IDbTransaction transaction = null,
+            bool buffered = true, int? commandTimeout = null, CommandType? commandType = null, object param = null)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            return connection.Query<T>(commandText, param, transaction, buffered, commandTimeout, commandType);
+        }
+
+        public IEnumerable<dynamic> Query(string commandText, IDbConnection connection, IDbTransaction transaction = null,
+            bool buffered = true, int? commandTimeout = null, CommandType? commandType = null, object param = null)
+        {
+            if (connection.State.Equals(ConnectionState.Closed))
+                connection.Open();
+
+            return connection.Query(commandText, param, transaction, buffered, commandTimeout, commandType);
+        }
+
+        public IEnumerable<object> Query(Type type, string commandText, IDbConnection connection,
+            IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = null,
+            CommandType? commandType = null, object param = null)
+        {
+            return connection.Query(type, commandText, param, transaction, buffered, commandTimeout, commandType);
+        }
+
+        public static IEnumerable<TReturn> Query<TFirst, TSecond, TReturn>(string commandText,
+            Func<TFirst, TSecond, TReturn> map, IDbConnection connection, IDbTransaction transaction = null,
+            bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null,
+            object param = null)
+        {
+            return connection.Query(commandText, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        public static IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(string commandText,
+            Func<TFirst, TSecond, TThird, TFourth, TFifth, TReturn> map, IDbConnection connection,
+            IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null,
+            CommandType? commandType = null, object param = null)
+        {
+            return connection.Query(commandText, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        public static IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn>(string commandText,
+            Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn> map, IDbConnection connection,
+            IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null,
+            CommandType? commandType = null, object param = null)
+        {
+            return connection.Query(commandText, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        public static IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TReturn>(string commandText,
+            Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TReturn> map, IDbConnection connection,
+            IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null,
+            CommandType? commandType = null, object param = null)
+        {
+            return connection.Query(commandText, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        public static IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TReturn>(string commandText,
+            Func<TFirst, TSecond, TThird, TFourth, TReturn> map, IDbConnection connection, IDbTransaction transaction = null,
+            bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null,
+            object param = null)
+        {
+            return connection.Query(commandText, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        public static IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TReturn>(string commandText,
+            Func<TFirst, TSecond, TThird, TReturn> map, IDbConnection connection, IDbTransaction transaction = null,
+            bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null,
+            object param = null)
+        {
+            return connection.Query(commandText, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        //public IEnumerable<TReturn> Query<TReturn>(string commandText, Type[] types, Func<object[], TReturn> map,
+        //    IDbConnection connection, IDbTransaction transaction = null, bool buffered = true,
+        //    string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null, dynamic param = null)
+        //{
+        //    if (connection.State.Equals(ConnectionState.Closed))
+        //        connection.Open();
+
+        //    return connection.Query<TReturn>(commandText, types, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+        //}
+        //public static SqlMapper.GridReader QueryMultiple(this IDbConnection cnn, CommandDefinition command);
+        //public static SqlMapper.GridReader QueryMultiple(this IDbConnection cnn, string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null);
     }
 }
