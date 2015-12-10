697bad723fdcee6ad1c03d5b3c0d2fa7e37786d5
wxll1120 <wxll1120@gmail.com>
2015-12-10 10:25:53 +0800

    新增待办事项用户控件

---
+++ CarManage/CarManage.UI.Client.Common/Task/TodoList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using ClassLibrary.ExceptionHandling;
+using ClassLibrary.Utility.Common;
+using ClassLibrary.Utility.Form;
+using CarManage.Configuration;
+using ClassLibrary.Winform.UI.Controls;
+using CarManage.Resources.Common;
+
+namespace CarManage.UI.Client.Common.Task
+{
+    public partial class TodoList : UserControlBase
+    {
+        public TodoList()
+        {
+            InitializeComponent();
+        }
+    }
+}
